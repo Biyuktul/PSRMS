6da896924cedaa10c3e38a7a4bd3f092678cb245
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-03-04 09:11:08 +0300

    commit 3

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,28 @@
 import '../styles/Main.css';
+import { SlBadge } from "react-icons/sl";
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 1,
+           
+            // marginTop: 50
+        }}
+    />
+);
 
-function Card() {
+function Card(props) {
+    const img_url = "https://images.pexels.com/photos/532001/pexels-photo-532001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+    const img_alt = Math.random()
     return (
-        <div className="card-container">
-            
+        <div class="card">
+            <div className='card-header'>
+            <SlBadge size={70}/>
+            <h1>DEPARTMENTS: 40</h1>
+            <ColoredLine color='white'/>
+            </div>
+            <img src={img_url} alt={img_alt}/>
         </div>
     );
 }
--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,6 +1,20 @@
+import Card from "./Card";
+import '../styles/Main.css'
+// const img_one = "https://images.pexels.com/photos/532001/pexels-photo-532001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+// const img_two = "";
+// const img_three = "";
+// const img_four = "";
+
 function Content() {
     return (
-        <h1>content</h1>
+        <div>
+            <div className="card-container">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
     );
 }
 
